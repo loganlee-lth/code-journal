080360fd383160ee14f1bf94e3f6be1fec49f1d5
Logan Taehwan Lee <leelogan93@gmail.com>
2023-05-30 04:08:00 +0000

    Add a submit event listener to the form. Ref #1

--- js/main.js
+++ js/main.js
@@ -1,7 +1,21 @@
 const $photoURL = document.querySelector('#photo-url');
 const $photoPreview = document.querySelector('#photo-preview');
+const $form = document.querySelector('form');
 
 $photoURL.addEventListener('input', event => {
   const url = event.target.value;
   $photoPreview.setAttribute('src', url);
+});
+
+$form.addEventListener('submit', event => {
+  event.preventDefault();
+  const entryJSON = {};
+  entryJSON.title = $form.elements.title.value;
+  entryJSON.photoURL = $form.elements.url.value;
+  entryJSON.notes = $form.elements.notes.value;
+  entryJSON.entryID = data.nextEntryId;
+  data.nextEntryId++;
+  data.entries.unshift(entryJSON);
+  $photoPreview.setAttribute('src', './images/placeholder-image-square.jpg');
+  $form.reset();
 });
